0e6c30fe685267f91f2a43ba7c668497e388ac47
Celine Davilmar <rdavi106@fiu.edu>
2022-07-11 22:48:20 -0400

    updated api

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".Weather-Search");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const inputVal = input.value;
+});
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,10 +1,22 @@
-var getWeatherRepos = function (lat, lon) {
+/* var getWeatherRepos = function (lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + { lat } + "&lon=" + { lon } + "&exclude={part}&appid={API key}";
 
-    // make a request to the url
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
             console.log(data);
         });
     });
-};
+}; */
+
+const apiKey = d3c6ad0c8ddd4caa5b1ae0d07f82e628;
+const inputVal = input.value;
+
+const appurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(() => {
+        console.log("Please search for a valid city 😩");
+    });

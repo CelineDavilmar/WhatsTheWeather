05ecbbce2cc947ea2abee17a16c22bff7866172d
Celine Davilmar <rdavi106@fiu.edu>
2022-07-12 21:42:59 -0400

    updated css

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,18 +1,9 @@
-/* var getWeatherRepos = function (lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + { lat } + "&lon=" + { lon } + "&exclude={part}&appid={API key}";
-
-    fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-}; */
 
 const apiKey = d3c6ad0c8ddd4caa5b1ae0d07f82e628;
 const inputVal = input.value;
 
-const appurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}`)
+const appurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${33.44}&lon=${-94.04}&exclude=hourly,daily&appid=${apiKey}`;
+fetch(appurl)
     .then(response => response.json())
     .then(data => {
         console.log(data);

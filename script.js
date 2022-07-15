188d6f1cf338cd0f27b5465c1198df7e18f6dc4e
Celine Davilmar <rdavi106@fiu.edu>
2022-07-14 21:33:18 -0400

    updates

--- script.js
+++ script.js
@@ -2,35 +2,61 @@ const apiKey = "d3c6ad0c8ddd4caa5b1ae0d07f82e628";
 const inputVal = input.value;
 
 const appurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${33.44}&lon=${-94.04}&exclude=hourly,daily&appid=${apiKey}`;
-fetch(appurl)
-    .then(response => response.json())
-    .then(data => {
-        const { main, city, sys, weather } = data;
+
+var weatherApp = {
+    getWeather: function (city) {
+        fetch(appurl)
+            .then(response => response.json())
+            .then(data => this.showWeatherApp(data));
+    },
+    showWeatherApp: function (data) {
+        const { main, name } = data;
         const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]
             }@2x.png`;
+        const description = data.weather[0]
+        const { humidity, temp } = data.main;
 
-        const li = document.createElement("li");
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".humidity").innerText = humidity + "%";
+        document.querySelector(".wind").innerText = speed + "mph";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+
+
+
+        //const li = document.createElement("li");
         li.classList.add("city");
         const markup = `
         <div class="weather-component">
-        <h2 data-name="${city},${sys.country}">
-            <span>${city}</span>
+        <h2 data-name="${name},${sys.country}">
+            <span>${name}</span>
             <sup>${sys.country}</sup>
         </h2>
+        <div>${description}</div>
         <div>${Math.round(main.temp)}<sup>°C</sup>
+        <div>${humidity}<sup>°C</sup>
+        <div>${temp}<sup>°C</sup>
         </div>
         <figure>
             <img src=${icon} alt=${weather[0]["main"]}>
             <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
         </div>
-`;
+        `;
         li.innerHTML = markup;
         list.appendChild(li);
-    })
-    .catch(() => {
-        console.log("Please search for a valid city!");
-    });
+    },
+    submit: function () {
+        this.showWeatherApp(document.querySelector().value)
+    }
+};
+document.getElementByClass("submit").addEventListener("click", function () {
+    weather.submit();
+    /* .catch (() => {
+    console.log("Please search for a valid city!"); */
+});
 
 
 const form = document.querySelector(".Weather-Search");
@@ -40,3 +66,5 @@ form.addEventListener("submit", e => {
     const listItems = list.querySelectorAll(".city");
     const inputVal = input.value;
 });
+
+
